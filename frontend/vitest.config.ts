365571614a0466a8192ec9b0709e36836a7a0ae7
stormozov <web@stormozov.ru>
2026-05-19 18:50:43 +0300

    test: reduce test cov threshold from 80 to 60

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -55,10 +55,10 @@ export default defineConfig({
         "routes/**",
       ],
       thresholds: {
-        branches: 80,
-        functions: 80,
-        lines: 80,
-        statements: 80,
+        branches: 60,
+        functions: 60,
+        lines: 60,
+        statements: 60,
       },
     },
 
